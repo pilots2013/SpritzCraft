9c46d4bf12d42a615d4f6c10f40af9a1544b1ebb
danieleromano1995 <71589744+danieleromano1995@users.noreply.github.com>
2021-10-12 21:57:19 +0200

    ok

--- SpritzCraft Playground.playground/Pages/Intro.xcplaygroundpage/Contents.swift
+++ SpritzCraft Playground.playground/Pages/Intro.xcplaygroundpage/Contents.swift
@@ -3,24 +3,12 @@ import PlaygroundSupport
 import SpriteKit
 import CoreGraphics
 
-var man = Character("Prince")
-var woman = Character("Princess")
+
 
 
 class IntroScene : SKScene {
     override func didMove(to view: SKView) {
        
-        let background = SKSpriteNode(imageNamed:"Forest")
-        background.zPosition = 1
-        background.size = CGSize(width: self.frame.size.width, height: self.frame.size.height)
-        background.position = CGPoint(x: 0, y: 0)
-        addChild(background)
-        man.position(x: Float(background.size.width)/2, y: -200)
-        woman.position(x: -Float(background.size.width)/2, y: -200)
-        addChild(man.character)
-        addChild(woman.character)
-        man.walk(x: -500, y: 0, duration: 6)
-        woman.walk(x: 500, y: 0, duration: 6)
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -28,7 +16,7 @@ class IntroScene : SKScene {
     }
 }
 
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 800, height: 600))
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 1920, height: 1080))
 
 if let scene = IntroScene(fileNamed: "IntroScene") {
     // Set the scale mode to scale to fit the window
