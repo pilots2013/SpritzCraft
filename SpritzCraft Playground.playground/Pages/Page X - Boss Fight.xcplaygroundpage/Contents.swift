7654b86f9c824c3be562a6d551499655b8feac85
pilots2013 <56370765+pilots2013@users.noreply.github.com>
2021-10-13 15:38:29 +0200

    Crystals and Health Bar implementation

--- SpritzCraft Playground.playground/Pages/Page X - Boss Fight.xcplaygroundpage/Contents.swift
+++ SpritzCraft Playground.playground/Pages/Page X - Boss Fight.xcplaygroundpage/Contents.swift
@@ -4,17 +4,28 @@ import PlaygroundSupport
 import SpriteKit
 import AVKit
 
-let patterSplitCount: UInt32 = 30
+let patterSplitCount: UInt32 = 40
+
+// BIT MASKS FOR COLLISIONS
 let PlayerCategory: UInt32 = 0x1 << 0
 let WallCategory: UInt32 = 0x2 << 1
 let BulletCategory: UInt32 = 0x3 << 2
 
+//TEXTURES FOR SPRITES
 var beast_rmove: [SKTexture]
 var fireball = SKTexture(imageNamed: "Fireball")
 var fireball_blue = SKTexture(imageNamed: "Fireball Blue")
 var rock = SKTexture(imageNamed: "Rock")
 
 
+var hp_0 = SKTexture(imageNamed: "hp_0")
+var hp_1 = SKTexture(imageNamed: "hp_1")
+var hp_2 = SKTexture(imageNamed: "hp_2")
+var hp_3 = SKTexture(imageNamed: "hp_3")
+var hp_4 = SKTexture(imageNamed: "hp_4")
+var hp_5 = SKTexture(imageNamed: "hp_5")
+
+var hp_textures: [SKTexture] = [hp_0, hp_1, hp_2, hp_3, hp_4, hp_5]
 var beast_texture = SKTexture(imageNamed: "beast")
 var beast_texture_r = SKTexture(imageNamed: "beast_r")
 var beast_texture_spec = SKTexture(imageNamed: "beast_spec")
@@ -35,7 +46,10 @@ class BossScene: SKScene, SKPhysicsContactDelegate {
     private var player: SKSpriteNode!
     private var mage: SKSpriteNode!
     private var touch: UITouch?
+    private var health_bar: SKSpriteNode!
+    private var health_points: Int = 5
     private var isInvincible: Bool = false
+    private var isPowered: Bool = false
     private var bullets: [SKTexture] = [fireball, fireball_blue, rock]
     private var audioPlayer: AudioPlayerImpl!
     private var audioSecondary: AudioPlayerImpl!
@@ -52,10 +66,13 @@ class BossScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.contactDelegate = self
         background.position = CGPoint(x: 0, y: 0)
         addChild(background)
+        health_bar = SKSpriteNode(texture: hp_5)
+        health_bar.setScale(0.2)
+        health_bar.position = CGPoint(x:frame.size.width/2 - 200, y:frame.size.height/2 - 40)
         player = SKSpriteNode(texture: beast_texture)
         player.setScale(0.3)
         player.name = "player"
-        player.position = CGPoint(x: 0, y:-frame.size.height/2 + 50)
+        player.position = CGPoint(x: 0, y:-frame.size.height/2 + 60)
         let playerBody = SKPhysicsBody(circleOfRadius: player.xScale * player.size.width/2)
         player.physicsBody = playerBody
         player.physicsBody?.categoryBitMask = PlayerCategory
@@ -75,7 +92,8 @@ class BossScene: SKScene, SKPhysicsContactDelegate {
         borderBody.isDynamic = false
         mage = SKSpriteNode(imageNamed:"mage")
         mage.setScale(2)
-        mage.position = CGPoint(x: 0, y:frame.size.height/2 - 50)
+        mage.position = CGPoint(x: 0, y:frame.size.height/2 - 80)
+        addChild(health_bar)
         addChild(mage)
         addChild(player)
         
@@ -120,6 +138,29 @@ class BossScene: SKScene, SKPhysicsContactDelegate {
         audioPlayer.play(effect: Audio.EffectFiles.fireball)
         
     }
+    
+    
+    func spawnCrystal(enemy: SKSpriteNode) {
+       
+        
+        let crystal = SKSpriteNode(imageNamed: "crystal")
+        crystal.zPosition = 1
+        crystal.name = "crystal"
+        crystal.setScale(0.4)
+        crystal.position = CGPoint(x: enemy.position.x, y: enemy.position.y)
+        
+        let action = SKAction.moveTo(y: -self.size.height/2, duration: 2)
+        let actionDone = SKAction.removeFromParent()
+        crystal.run(SKAction.sequence([action, actionDone]))
+
+        crystal.physicsBody = SKPhysicsBody(circleOfRadius: crystal.frame.height/2)
+            
+        crystal.physicsBody?.categoryBitMask = BulletCategory
+        crystal.physicsBody?.collisionBitMask = 0x0
+        crystal.physicsBody?.contactTestBitMask = PlayerCategory
+        self.addChild(crystal)
+        
+    }
     func bosspattern() {
         let wait = SKAction.wait(forDuration: 1)
         
@@ -127,7 +168,7 @@ class BossScene: SKScene, SKPhysicsContactDelegate {
             self.spawnBullet(enemy: self.mage)
            }
         let moverl = SKAction.sequence([.moveTo(x:-frame.size.width/2 + 50, duration:3), .moveTo(x:frame.size.width/2 - 50, duration:3)])
-        let moveud = SKAction.sequence([.moveTo(y:frame.size.height/2 - 70, duration:0.4), .moveTo(y:frame.size.height/2 - 30, duration:0.4)])
+        let moveud = SKAction.sequence([.moveTo(y:frame.size.height/2 - 120, duration:0.4), .moveTo(y:frame.size.height/2 - 80, duration:0.4)])
         mage.run(.repeatForever(moverl))
         mage.run(.repeatForever(moveud))
         mage.run(SKAction.repeatForever(SKAction.sequence([wait, run])))
@@ -140,7 +181,7 @@ class BossScene: SKScene, SKPhysicsContactDelegate {
            }
         let slide = SKAction.moveTo(x: frame.size.width/2 - 50, duration: 0.2)
         let moverl = SKAction.sequence([.moveTo(x:frame.size.width/2 - 50, duration:2), .moveTo(x:-frame.size.width/2 + 50, duration:2)])
-        let moveud = SKAction.sequence([.moveTo(y:frame.size.height/2 - 70, duration:0.4), .moveTo(y:frame.size.height/2 - 30, duration:0.4)])
+        let moveud = SKAction.sequence([.moveTo(y:frame.size.height/2 - 120, duration:0.4), .moveTo(y:frame.size.height/2 - 80, duration:0.4)])
         mage.run(slide)
         mage.run(.repeatForever(moverl))
         mage.run(.repeatForever(moveud))
@@ -197,6 +238,10 @@ class BossScene: SKScene, SKPhysicsContactDelegate {
             bosspattern()
             bulletCount = (bulletCount + 1) % patterSplitCount
         }
+        if(bulletCount == 20) {
+            spawnCrystal(enemy: mage)
+            bulletCount = (bulletCount + 1) % patterSplitCount
+        }
             
     }
     func didBegin(_ contact: SKPhysicsContact) {
@@ -208,19 +253,33 @@ class BossScene: SKScene, SKPhysicsContactDelegate {
               isInvincible = false
             }
             let hurt_action = SKAction.animate(with:beast_hurt_anim, timePerFrame: 0.1)
+            self.health_points -= 1
+            if(self.health_points >= 0) {
+                let hp_loss_action = SKAction.setTexture(hp_textures[health_points])
+                health_bar.run(hp_loss_action)
+            }
+            
+            
             player.removeAllActions()
+            
             player.run(hurt_action)
             audioSecondary.effectsVolume = 0.5
             audioSecondary.play(effect: Audio.EffectFiles.hurt)
             
             print("DANNO")
         }
+        if(contact.bodyA.node?.name == "player" && contact.bodyB.node?.name == "crystal" && !isPowered) {
+            isPowered = true
+            player.colorBlendFactor = 0.6
+            player.color = UIColor.systemGreen
+            print("POWAHHH!!!")
+        }
     }
     
 }
 
 // Load the SKScene from 'GameScene.sks'
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: UIScreen().bounds.width, height: UIScreen().bounds.height))
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 1920, height: 1080))
 
 if let scene = BossScene(fileNamed: "BossScene") {
     // Set the scale mode to scale to fit the window
