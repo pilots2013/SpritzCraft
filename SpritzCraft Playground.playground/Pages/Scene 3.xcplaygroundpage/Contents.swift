443bd5360afd68fa6a108c02a91c924fa0e25514
danieleromano1995 <71589744+danieleromano1995@users.noreply.github.com>
2021-10-14 14:00:58 +0200

    Scene 3

--- SpritzCraft Playground.playground/Pages/Scene 3.xcplaygroundpage/Contents.swift
+++ SpritzCraft Playground.playground/Pages/Scene 3.xcplaygroundpage/Contents.swift
@@ -6,23 +6,21 @@ import CoreGraphics
 
 
 
-class IntroScene : SKScene {
+class Scene3 : SKScene {
     override func didMove(to view: SKView) {
        
     }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let scene1 = Scene1(fileNamed: "Scene1")
-        scene1?.scaleMode = .aspectFit
-        sceneView.presentScene(scene1)
+        let scene = SceneGood(fileNamed: "Scene3 Good")
+        scene?.scaleMode = .aspectFit
+        sceneView.presentScene(scene)
     }
-    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
 }
 
-class Scene1 : SKScene {
+class SceneGood : SKScene{
     override func didMove(to view: SKView) {
        
     }
@@ -34,13 +32,12 @@ class Scene1 : SKScene {
 
 let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 1920, height: 1080))
 
-if let scene = IntroScene(fileNamed: "IntroScene") {
+if let scene = Scene3(fileNamed: "Scene3") {
     // Set the scale mode to scale to fit the window
     scene.scaleMode = .aspectFit
+    
     // Present the scene
     sceneView.presentScene(scene)
 }
 
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
-
-
--- SpritzCraft Playground.playground/Scene 3.xcplaygroundpage/Contents.swift
+++ SpritzCraft Playground.playground/Scene 3.xcplaygroundpage/Contents.swift
@@ -6,22 +6,6 @@ import CoreGraphics
 
 
 
-class IntroScene : SKScene {
-    override func didMove(to view: SKView) {
-       
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let scene1 = Scene1(fileNamed: "Scene1")
-        scene1?.scaleMode = .aspectFit
-        sceneView.presentScene(scene1)
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-}
-
 class Scene1 : SKScene {
     override func didMove(to view: SKView) {
        
@@ -34,13 +18,12 @@ class Scene1 : SKScene {
 
 let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 1920, height: 1080))
 
-if let scene = IntroScene(fileNamed: "IntroScene") {
+if let scene = Scene1(fileNamed: "Scene1") {
     // Set the scale mode to scale to fit the window
-    scene.scaleMode = .aspectFit
+    scene.scaleMode = .resizeFill
+    
     // Present the scene
     sceneView.presentScene(scene)
 }
 
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
-
-
