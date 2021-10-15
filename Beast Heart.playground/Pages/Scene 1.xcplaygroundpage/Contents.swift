0c5e43a167d5e9e79fa213a7ab59e0d14ce31692
danieleromano1995 <71589744+danieleromano1995@users.noreply.github.com>
2021-10-15 11:33:08 +0200

    Aspect fit scene 1

--- Beast Heart.playground/Pages/Scene 1.xcplaygroundpage/Contents.swift
+++ Beast Heart.playground/Pages/Scene 1.xcplaygroundpage/Contents.swift
@@ -46,7 +46,7 @@ struct MenuView : View {
                         let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 1920, height: 1080))
                         if let scene = Scene1(fileNamed: "Scene1") {
                             // Set the scale mode to scale to fit the window
-                            scene.scaleMode = .resizeFill
+                            scene.scaleMode = .aspectFit
                             
                             // Present the scene
                             sceneView.presentScene(scene)
